5e45e2fb8c83445b9a674d7e6945df2dc5c7a822
Diogo Pereira <diogo.pereira.ee@gmail.com>
2023-05-03 11:39:30 +0100

    added a non-blocking example with poll()

--- sources/main.cpp
+++ sources/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dsilveri <dsilveri@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dsilveri <dsilveri@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 11:52:16 by dsilveri          #+#    #+#             */
-/*   Updated: 2023/04/17 15:17:42 by dsilveri         ###   ########.fr       */
+/*   Updated: 2023/05/03 11:35:41 by dsilveri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
+
+#include <sys/poll.h>
 
 
 // Test comminication wit sockets
@@ -29,6 +31,10 @@
 
 #define PORT 8080
 
+void send_response(int socket_fd);
+
+
+/*
 int main(void)
 {
 	int server_fd, new_socket;
@@ -36,16 +42,16 @@ int main(void)
 	struct sockaddr_in address;
 	int addrlen;
 
-	std::string head;
-	std::string body;
-	std::string res;
+	struct pollfd fds[10];
 	
+	// cria socket
 	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
 	{
 		perror("cannot create socket");
 		return 0; 
 	}
 
+	// para desimpedir o porto
 	int enable = 1;
 	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0)
 	{
@@ -69,6 +75,8 @@ int main(void)
         perror("In listen");
         exit(EXIT_FAILURE);
     }
+
+	char buffer[30000] = {0};
     while(1)
     {
         printf("\n+++++++ Waiting for new connection ++++++++\n\n");
@@ -77,31 +85,119 @@ int main(void)
             perror("In accept");
             exit(EXIT_FAILURE);
         }
-        
-        char buffer[30000] = {0};
-		
         valread = read(new_socket , buffer, 30000);
-        
-		std::cout << "read: " << valread << std::endl;
+		std::cout << valread << std::endl;
 
-		body += "<html><head><title>WebServer</title></head><body><h1>Hello World</h1></body></html>";
-		// body += "\r\n\r\n";
+		send_response(new_socket);
 
-		head += "HTTP/1.1 200 OK\r\nContent-length: ";
-		head += "85\r\n";
-		// head += body.size() + "\r\n\r\n";
-		head += "Content-Type: text/html\r\n\r\n";
-
-		res = head + body + "\r\n\r\n";
-
-		//send(new_socket, "HTTP/1.1 200 OK\r\nContent-length: 5\r\n\r\nhello\r\n\r\n", 48, 0);
-		send(new_socket, res.c_str(), res.size(), 0);
-		std::cout << "buffer:\n" << buffer << std::endl;
-        close(new_socket);
-		res.clear();
-		body.clear();
-		head.clear();
+		poll(fds, 10, 0);
     }
 
 	return (0);
+}
+*/
+
+
+int main(void)
+{
+	int server_fd, new_socket;
+	long valread;
+	struct sockaddr_in address;
+	int addrlen;
+
+	struct pollfd fds[10];
+	
+	// cria socket
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+	{
+		perror("cannot create socket");
+		return 0; 
+	}
+
+	// para desimpedir o porto
+	int enable = 1;
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0)
+	{
+		close(server_fd);
+		return (EXIT_FAILURE);
+	}
+
+	memset((char *)&address, 0, sizeof(address)); 
+	address.sin_family = AF_INET; 
+	address.sin_addr.s_addr = htonl(INADDR_ANY); 
+	address.sin_port = htons(PORT);
+	addrlen = sizeof(address);
+	
+	if (bind(server_fd, (struct sockaddr *) &address, sizeof(address)) < 0) 
+	{
+    	perror("bind failed"); 
+    	return 0; 
+	}
+    if (listen(server_fd, 10) < 0)
+    {
+        perror("In listen");
+        exit(EXIT_FAILURE);
+    }
+
+	char buffer[30000] = {0};
+
+
+	fds[0].fd = server_fd;
+	fds[0].events = POLLIN;
+
+    while(1)
+    {
+        printf("\n+++++++ Waiting for new connection ++++++++\n\n");
+
+		poll(fds, 10, 100);
+
+		// POLLERR, POLLHUP, or POLLNVAL
+		if (fds[0].revents && POLLIN)
+		{
+			if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
+			{
+				perror("In accept");
+				exit(EXIT_FAILURE);
+			}
+			valread = read(new_socket , buffer, 30000);
+			std::cout << valread << std::endl;
+
+			send_response(new_socket);		
+		}
+		
+
+        /*
+		if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
+        {
+            perror("In accept");
+            exit(EXIT_FAILURE);
+        }
+        valread = read(new_socket , buffer, 30000);
+		std::cout << valread << std::endl;
+
+		send_response(new_socket);
+		*/
+
+		
+    }
+
+	return (0);
+}
+
+void send_response(int socket_fd)
+{
+	std::string head;
+	std::string body;
+	std::string res;
+
+	body += "<html><head><title>WebServer</title></head><body><h1>Hello World</h1></body></html>";
+	head += "HTTP/1.1 200 OK\r\nContent-length: ";
+	head += "85\r\n";
+	head += "Content-Type: text/html\r\n\r\n";
+	res = head + body + "\r\n\r\n";
+
+	send(socket_fd, res.c_str(), res.size(), 0);
+	
+    if (close(socket_fd) == -1)
+		std::cout << "Erro a fechar" << std::endl;
 }
