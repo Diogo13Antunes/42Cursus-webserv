d2c11bc122b3bbdf6609f63a0a27803dd11847e3
Diogo Pereira <diogo.pereira.ee@gmail.com>
2023-05-23 16:36:06 +0100

    added simple example with epoll

--- sources/main.cpp
+++ sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dsilveri <dsilveri@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 11:52:16 by dsilveri          #+#    #+#             */
-/*   Updated: 2023/05/23 10:24:27 by dsilveri         ###   ########.fr       */
+/*   Updated: 2023/05/23 16:33:55 by dsilveri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@
 #include <netinet/in.h>
 #include <string.h>
 
-#include <sys/poll.h>
-
+//#include <sys/poll.h>
+#include <sys/epoll.h>
 #include <sstream>
-
 #include <ctime>
+#include <fcntl.h>
 
 #include "Connection.hpp"
 #include "Connections.hpp"
@@ -32,6 +32,7 @@
 
 #include "Messenger.hpp"
 
+#define PORT 8080
 
 
 // Test comminication wit sockets
@@ -42,10 +43,7 @@
 
 // server code
 
-#define PORT 8080
-
-void send_response(int socket_fd);
-
+/*
 
 int main(void)
 {
@@ -161,17 +159,123 @@ int main(void)
 	return (0);
 }
 
+*/
 
-/*
+void send_response_test1(int socket_fd)
+{
+	std::string head;
+	std::string body;
+	std::string res;
+
+	std::stringstream out;
+
+	body = "<html><head><title>WebServer</title></head><body><h1>Hello World</h1></body></html>\r\n\r\n";
+	out << body.size();
+	
+	head = "HTTP/1.1 200 OK\r\nContent-length: ";
+	head += out.str();
+	head += "\r\n";
+	head += "Content-Type: text/html\r\n\r\n";
+	res = head + body;
+	send(socket_fd, res.c_str(), res.size(), 0);
+}
+
+
 int main(void)
 {
-	EventLoop			eventLoop;
-	EventHandlerFactory	factory;
+	int server_fd, new_socket;
+	struct sockaddr_in address;
+	int addrlen;
 
-	eventLoop.registerEvent(factory.getEventHandler(READ_EVENT));
-	eventLoop.registerEvent(factory.getEventHandler(WRITE_EVENT));
-	eventLoop.handleEvents();
+	
+	// cria socket
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+	{
+		perror("cannot create socket");
+		return 0; 
+	}
 
+	// para desimpedir o porto
+	int enable = 1;
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0)
+	{
+		close(server_fd);
+		return (EXIT_FAILURE);
+	}
+
+
+	memset((char *)&address, 0, sizeof(address)); 
+	address.sin_family = AF_INET; 
+	address.sin_addr.s_addr = htonl(INADDR_ANY); 
+	address.sin_port = htons(PORT);
+	addrlen = sizeof(address);
+	
+	if (bind(server_fd, (struct sockaddr *) &address, sizeof(address)) < 0) 
+	{
+    	perror("bind failed"); 
+    	return 0; 
+	}
+    if (listen(server_fd, 100) < 0)
+    {
+        perror("In listen");
+        exit(EXIT_FAILURE);
+    }
+
+    int epollFd = epoll_create1(0);
+    if (epollFd == -1) {
+        std::cerr << "Failed to create epoll instance." << std::endl;
+        return 1;
+    }
+
+	epoll_event event;
+    event.data.fd = server_fd;
+    event.events = EPOLLIN | EPOLLET;
+    if (epoll_ctl(epollFd, EPOLL_CTL_ADD, server_fd, &event) == -1) {
+        std::cerr << "Failed to add server socket to epoll." << std::endl;
+        return 1;
+    }
+
+	std::cout << "valor do epoll_fd " << epollFd << std::endl;
+
+	int max_events = 1024;
+	epoll_event events[max_events];
+	int numEvents;
+
+	char	buffer[30000];
+	ssize_t	valread;
+	int flags;
+
+    while(1)
+    {
+		numEvents = epoll_wait(epollFd, events, max_events, 1500);
+		for (int i = 0; i < numEvents; i++) 
+		{
+			if (events[i].data.fd == server_fd)
+			{
+				if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
+				{
+					perror("In accept");
+					exit(EXIT_FAILURE);
+				}
+				event.data.fd = new_socket;
+                event.events = EPOLLIN | EPOLLET;
+				// Set client socket to non-blocking mode
+            	flags = fcntl(new_socket, F_GETFL, 0);
+                fcntl(new_socket, F_SETFL, flags | O_NONBLOCK);
+                epoll_ctl(epollFd, EPOLL_CTL_ADD, new_socket, &event); // has to verify error returned
+			}
+			else 
+			{
+				if (events[i].events & EPOLLIN)
+				{
+						for(int i = 0; i < 30000; i++)
+							buffer[i] = 0;
+						valread = read(events[i].data.fd, buffer, 30000 - 1);
+						std::cout << buffer << std::endl;
+						send_response_test1(events[i].data.fd);
+				}
+			}
+		}
+	}
 	return (0);
 }
-*/
