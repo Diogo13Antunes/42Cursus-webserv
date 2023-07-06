7f88dc1124f4a089b7868c324bd4353ab5eb3b22
Diogo13Antunes <diogoandrecandeias@gmail.com>
2023-07-06 14:49:33 +0100

    Deleted all main files, because of conflicting code with others brunchs.

--- sources/minificator/MinificatorHTML.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "AMinificator.hpp"
-
-class MinificatorHTML: public AMinificator
-{
-	public:
-		static std::string	getMinificatedHTML(const char *file);
-};
--- sources/minificator/MinificatorJS.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "AMinificator.hpp"
-
-class MinificatorJS: public AMinificator
-{
-	public:
-		static std::string	getMinificatedJS(const char *file);
-};
