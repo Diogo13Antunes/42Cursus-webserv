500fcf45733137b9d6a663f125dce96fc5bc9a6a
Diogo13Antunes <diogoandrecandeias@gmail.com>
2023-08-23 17:53:38 +0100

    Refactoring in CGI Response

--- sources/handleRes/CgiResponseProcess.hpp
+++ sources/handleRes/CgiResponseProcess.hpp
@@ -4,18 +4,20 @@
 #include <iostream>
 #include "IStateRes.hpp"
 #include "StringUtils.hpp"
+#include "TimeDate.hpp"
 
 class CgiResponseProcess: public IStateRes
 {
 	private:
-		std::vector<std::string>	_getHeaderVector(std::string &src);
-		std::string					_getKey(std::string &line);
-		std::string					_getValue(std::string &line);
-		std::string					_invalidCgiResponse(void);
-		std::string					_getStatusLine(std::vector<std::string> &src);
-		bool						_existContent(std::vector<std::string> &header);
-		std::string					_getCgiBody(std::string &src);
-		std::string					_getResponseHeaderLine(std::string &src);
+		std::map<std::string, std::string>	_getHeaderMap(std::string &src);
+		std::string							_getKey(std::string &line);
+		std::string							_getValue(std::string &line);
+		std::string							_getStatusLine(std::map<std::string, std::string> &src);
+		bool								_existContent(std::map<std::string, std::string> &header);
+		std::string							_getCgiBody(std::string &src);
+		std::string							_getResponseHeaderLine(std::string key, std::string value);
+		std::pair<std::string, std::string>	_makePair(std::string &line);
+		void								_completeHeaderSet(std::map<std::string, std::string> &header);
 
 	public:
 		CgiResponseProcess(void);
