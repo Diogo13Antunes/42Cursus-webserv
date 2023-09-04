b34ec36d9e5c63ed70a151922cfa35974b65d32e
Diogo Pereira <diogo.pereira.ee@gmail.com>
2023-09-04 18:24:24 +0100

    Refactored InitialState of response handler.

--- sources/handleRes/InitialState.cpp
+++ sources/handleRes/InitialState.cpp
@@ -6,7 +6,7 @@
 /*   By: dsilveri <dsilveri@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/24 17:51:44 by dsilveri          #+#    #+#             */
-/*   Updated: 2023/09/04 17:34:34 by dsilveri         ###   ########.fr       */
+/*   Updated: 2023/09/04 18:22:13 by dsilveri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,82 +20,41 @@ InitialState::InitialState(void) {}
 
 InitialState::~InitialState(void) {}
 
-/*
+
 StateResType InitialState::handle(Event *event, ServerConfig& config)
 {
-	std::string reqPath;
-	std::string resourcePath;
-	std::string	method;
-	int			acceptMethod;
-
-	method = event->getReqLineMethod();
+	std::string	reqPath;
+	std::string realPath;
+	std::string	route;
+	
 	if (event->getStatusCode())
 		return (ERROR_HANDLING);
 	if (!event->getCgiScriptResult().empty())
 		return (CGI_RES_HANDLING);
-	if (_hasRedirection(event, config))
+	reqPath = event->getReqLinePath();
+	route = _getRouteName(config, reqPath);
+	realPath = _getRealPath(config, reqPath, route);
+	if (_hasForcedRedirection(event, reqPath, realPath, route) || _hasConfRedirection(event, config, route))
 		return (REDIRECTION_HANDLING);
-
-	reqPath =  event->getReqLinePath();
-	acceptMethod = config.isLocationAcceptedMethod(reqPath, method);
-	if (!acceptMethod)
-	{
-		event->setStatusCode(405);
+	realPath = _getPathWithIndex(config, realPath, route);
+	event->setResourcePath(realPath);
+	if (!_isValidFile(event, realPath))
 		return (ERROR_HANDLING);
-	}
-
-	
-	resourcePath = _getResourceFromURLPath(config, reqPath);
-	event->setResourcePath(resourcePath);
-	if (_isFolder(resourcePath))
-		return (DIRECTORY_LISTING);
-	if (reqPath.at(reqPath.size() - 1) != '/' && access(resourcePath.c_str(), F_OK))
-	{
-		resourcePath = _getPreviousRouteResourcePath(config, reqPath);
-		event->setResourcePath(resourcePath);
-	}
-	if (access(resourcePath.c_str(), F_OK))
-	{
-		event->setStatusCode(NOT_FOUND_CODE);
+	if (!_isValidMethod(event, config, route))
 		return (ERROR_HANDLING);
-	}
-	if (access(resourcePath.c_str(), R_OK))
+	if (_isFolder(realPath))
 	{
-		event->setStatusCode(FORBIDEN_CODE);
-		return (ERROR_HANDLING);
+		if (!config.getLocationAutoIndex(route).compare("on"))
+			return (DIRECTORY_LISTING);
+		else
+		{
+			event->setStatusCode(FORBIDEN_CODE);
+			return (ERROR_HANDLING);	
+		}
 	}
 	return (STATIC_FILE_HANDLING);
 }
-*/
 
-/*
-bool InitialState::_hasRedirection(Event *event, ServerConfig& config)
-{
-	std::string	path;
-	std::string	resource;
-	int			code;
-	
-	code = 0;
-	path = event->getReqLinePath();
-	if (!code && path.at(path.size() - 1) != '/')
-	{
-		if (config.isConfiguredRoute(path) || _isFolder(config.getMasterRoot() + path))
-		{
-			resource = path + "/";
-			code = MOVED_PERMANENTLY;
-		}
-	}
-	if (!code)
-		config.getRedirectionInfo(path, code, resource);
-	if (code && !resource.empty())
-	{
-		event->setRredirectCode(code);
-		event->setRredirectResource(resource);
-		return (true);
-	}
-	return (false);
-}
-*/
 
 bool InitialState::_isFolder(std::string path)
 {
@@ -111,147 +70,6 @@ bool InitialState::_isFolder(std::string path)
 	return (false);
 }
 
-std::string InitialState::_getResourceFromURLPath(ServerConfig& config, std::string path)
-{
-    std::string fullPath;
-    std::string    rootPath;
-    std::string    aliasPath;
-    std::string    index;
-
-    aliasPath = config.getLocationAlias(path);
-    if (aliasPath.empty())
-        rootPath = config.getLocationRootPath(path);
-    if (rootPath.empty() && aliasPath.empty())
-        rootPath = config.getMasterRoot();
-    if (!aliasPath.empty())
-        fullPath = aliasPath;
-    else
-        fullPath = rootPath + path;
-    index = config.getLocationIndex(path);
-    if (!index.empty())
-        fullPath += index;
-    else
-    {
-        if (access((fullPath + "index.html").c_str(), F_OK) == 0)
-            fullPath += "index.html";
-    }
-    return (fullPath);
-}
-
-std::string InitialState::_getPreviousRouteResourcePath(ServerConfig& config, std::string path)
-{
-	std::string fileName;
-	std::string prevPath;
-	std::string	aliasPath;
-	size_t		idx;
-
-	idx = path.find_last_of("/");
-	if (idx != path.npos)
-		fileName = path.substr(idx + 1);
-	prevPath = _getPreviousRoute(path);
-	while (!prevPath.empty())
-	{
-		aliasPath = config.getLocationAlias(prevPath);
-		if (!aliasPath.empty())
-		{
-			aliasPath += fileName;
-			break;
-		}
-		prevPath = _getPreviousRoute(prevPath);
-	}
-	return (aliasPath);
-}
-
-std::string InitialState::_getPreviousRoute(std::string path)
-{
-	size_t		idx;
-
-	if (path.size() == 1)
-		return (std::string());
-	if (path.at(path.size() - 1) == '/')
-		path.erase(path.size() - 1);
-	idx = path.find_last_of("/");
-	if (idx != path.npos && idx > 0)
-		path.erase(idx);
-	else if (idx != path.npos)
-		return (std::string());
-	return (path);
-}
-
-
-
-
-// New Functions
-
-StateResType InitialState::handle(Event *event, ServerConfig& config)
-{
-	std::string	reqPath;
-	std::string realPath;
-	std::string	route;
-	std::string	method;
-	int			acceptMethod;
-
-	if (event->getStatusCode())
-		return (ERROR_HANDLING);
-	if (!event->getCgiScriptResult().empty())
-		return (CGI_RES_HANDLING);
-
-	reqPath = event->getReqLinePath();
-	route = _getRouteName(config, reqPath);
-	method = event->getReqLineMethod();
-	acceptMethod = config.isLocationAcceptedMethod(route, method);
-	realPath = _getRealPath(config, reqPath, route);
-	if (_hasForcedRedirection(event, reqPath, realPath, route) || _hasConfRedirection(event, config, route))
-		return (REDIRECTION_HANDLING);
-	realPath = _getPathWithIndex(config, realPath, route);
-	event->setResourcePath(realPath);
-
-	//Verificação de erros
-	if (!acceptMethod)
-	{
-		event->setStatusCode(NOT_ALLOWED_CODE);
-		return (ERROR_HANDLING);
-	}
-	else
-	{
-		if (!_isMethodImplemented(method))
-		{
-			event->setStatusCode(NOT_IMPLEMENTED_CODE);
-			return (ERROR_HANDLING);
-		}
-		else if (event->getCgiScriptResult().empty() && method.compare("GET"))
-		{
-			event->setStatusCode(FORBIDEN_CODE);
-			return (ERROR_HANDLING);
-		}
-	}
-	if (access(realPath.c_str(), F_OK))
-	{
-		event->setStatusCode(NOT_FOUND_CODE);
-		return (ERROR_HANDLING);
-	}
-	if (access(realPath.c_str(), R_OK))
-	{
-		event->setStatusCode(FORBIDEN_CODE);
-		return (ERROR_HANDLING);
-	}
-
-
-	if (_isFolder(realPath))
-	{
-		if (!config.getLocationAutoIndex(route).compare("on"))
-			return (DIRECTORY_LISTING);
-		else
-		{
-			event->setStatusCode(FORBIDEN_CODE);
-			return (ERROR_HANDLING);	
-		}
-	}
-
-	
-
-	return (STATIC_FILE_HANDLING);
-}
 
 std::string InitialState::_getPreviousPath(std::string path)
 {
@@ -359,4 +177,44 @@ bool InitialState::_hasConfRedirection(Event *event, ServerConfig& config, std::
 		return (true);
 	}
 	return (false);
+}
+
+bool InitialState::_isValidMethod(Event *event, ServerConfig& config, std::string route)
+{
+	std::string	method;
+	int			acceptMethod;
+	
+	method = event->getReqLineMethod();
+	acceptMethod = config.isLocationAcceptedMethod(route, method);
+	if (!acceptMethod)
+	{
+		event->setStatusCode(NOT_ALLOWED_CODE);
+		return (false);
+	}
+	if (!_isMethodImplemented(method))
+	{
+		event->setStatusCode(NOT_IMPLEMENTED_CODE);
+		return (false);
+	}
+	else if (event->getCgiScriptResult().empty() && method.compare("GET"))
+	{
+		event->setStatusCode(FORBIDEN_CODE);
+		return (false);
+	}
+	return (true);
+}
+
+bool InitialState::_isValidFile(Event *event, std::string path)
+{
+	if (access(path.c_str(), F_OK))
+	{
+		event->setStatusCode(NOT_FOUND_CODE);
+		return (false);
+	}
+	if (access(path.c_str(), R_OK))
+	{
+		event->setStatusCode(FORBIDEN_CODE);
+		return (false);
+	}
+	return (true);
 }
